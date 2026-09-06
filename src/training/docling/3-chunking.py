93690d978ba66192d25b6817363e4bdbabfd828c
Danilo Santos <dandesenvolvimento@gmail.com>
2026-09-06 14:21:49 -0300

    chunking done

--- src/training/docling/3-chunking.py
+++ src/training/docling/3-chunking.py
@@ -1,5 +1,10 @@
 from docling.document_converter import DocumentConverter
-from docling.chunking import HierarchicalChunker
+from docling.chunking import HybridChunker
+from docling_core.transforms.chunker.tokenizer.huggingface import HuggingFaceTokenizer
+from transformers import AutoTokenizer
+
+EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+MAX_TOKENS = 300
 
 converter = DocumentConverter()
 
@@ -7,8 +12,16 @@ result = converter.convert("./2408.09869v5.pdf")
 
 document = result.document
 
-chunker = HierarchicalChunker()
+tokenizer = HuggingFaceTokenizer(
+    tokenizer=AutoTokenizer.from_pretrained(EMBED_MODEL), max_tokens=MAX_TOKENS
+)
+
+chunker = HybridChunker(tokenizer=tokenizer, max_tokens=MAX_TOKENS, merge_peers=True)
 
 chunks = list(chunker.chunk(document))
+len(chunks)
 
-print(chunks[4].text)
+for i, chunk in enumerate(chunks):
+    print(f"==={i}===\n")
+    txt_tokens = tokenizer.count_tokens(chunk.text)
+    print(f"chunk.text ({txt_tokens} tokenss)")
